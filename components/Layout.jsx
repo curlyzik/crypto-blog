cd197ac6335474e11048fa0b185ec0792d899bb2
curlyzik <curlyzik@gmail.com>
2021-10-04 15:22:52 +0100

    Built out the homepage and cryptopage

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 import { Layout as Layouts } from "antd";
 import Navbar from "./Navbar";
 import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 
 const { Header, Content } = Layouts;
-const [collapsed, setCollapsed] = useState(false);
-const toggle = () => setCollapsed(!collapsed);
 
 const Layout = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const toggle = () => setCollapsed(!collapsed);
   return (
     <Layouts>
-      <Navbar />
+      <Navbar collapsed={collapsed} />
       <Layouts>
         <Header className="bg-white flex items-center">
           <div onClick={toggle} className="text-xl">
@@ -24,7 +23,7 @@ const Layout = ({ children }) => {
           </div>
         </Header>
 
-        <Content className="m-5 bg-white p-4">{children}</Content>
+        <Content className="m-5 bg-white p-7">{children}</Content>
       </Layouts>
     </Layouts>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Layout, Menu } from "antd";
 import Link from "next/link";
 import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
+  FundOutlined,
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
 } from "@ant-design/icons";
 
 const { Sider } = Layout;
 
-const Navbar = () => {
+const Navbar = ({ collapsed }) => {
   return (
     <>
       <Sider
@@ -19,26 +20,42 @@ const Navbar = () => {
         collapsed={collapsed}
         className="h-screen"
       >
-        <div className="text-3xl text-gray-900">Logo</div>
+        <div className="text-3xl text-gray-900 p-5 ">Logo</div>
         <Menu theme="" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
+          <Menu.Item
+            key="1"
+            icon={<HomeOutlined />}
+            className="flex items-center"
+          >
             <Link href="/">
               <a>Home</a>
             </Link>
           </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            <Link href="/news">
-              <a>News</a>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            <Link href="/exchanges">
-              <a>Exhnages</a>
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
+          <Menu.Item
+            key="2"
+            icon={<FundOutlined />}
+            className="flex items-center"
+          >
             <Link href="/cryptocurrencies">
               <a>Cryptocurrencies</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item
+            key="3"
+            icon={<MoneyCollectOutlined />}
+            className="flex items-center"
+          >
+            <Link href="/exchanges">
+              <a>Exchanges</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item
+            key="4"
+            icon={<BulbOutlined />}
+            className="flex items-center"
+          >
+            <Link href="/cryptocurrencies">
+              <a>News</a>
             </Link>
           </Menu.Item>
         </Menu>
